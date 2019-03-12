2dd907671b634fcd3e2be9f85c6852deab8d53de
Anet <curly160795@gmail.com>
2019-03-12 18:33:57 +0200

    Config for dataTable was added

---
+++ src/Common/Config.jsx
@@ -0,0 +1,164 @@
+export const peopleColumnConfig = {
+  name: {
+    title: 'Name',
+    isSortable: true,
+    isSearchable: true,
+    link: '/person/:name',
+  },
+  mass: {
+    title: 'Mass',
+    isSortable: true,
+  },
+  hairColor: {
+    title: 'Hair_Color',
+  },
+  born: {
+    title: 'Born',
+    isSortable: true,
+  },
+  gender: {
+    title: 'Gender',
+  },
+  films: {
+    title: 'Films',
+  },
+  species: {
+    title: 'Species',
+  },
+};
+
+export const filmsColumnConfig = {
+  name: {
+    title: 'Title',
+    isSortable: true,
+    isSearchable: true,
+  },
+  episode: {
+    title: 'Episode Id',
+    isSortable: true,
+  },
+  opening_crawl: {
+    title: 'Opening_crawl',
+    isSearchable: true,
+  },
+  director: {
+    title: 'Director',
+    isSortable: true,
+  },
+  producer: {
+    title: 'Producer',
+    isSortable: true,
+  },
+  release_date: {
+    title: 'Release Date',
+  },
+};
+
+export const planetsColumnConfig = {
+  name: {
+    title: 'Name',
+    isSortable: true,
+    isSearchable: true,
+  },
+  diameter: {
+    title: 'Diameter',
+    isSortable: true,
+  },
+  rotation_period: {
+    title: 'Rotation Period',
+    isSortable: true,
+  },
+  orbital_period: {
+    title: 'Orbital Period',
+    isSortable: true,
+  },
+  gravity: {
+    title: 'Gravity',
+    isSortable: true,
+  },
+  population: {
+    title: 'Population',
+    isSortable: true,
+  },
+  climate: {
+    title: 'Climate',
+    isSearchable: true,
+  },
+  surface_water: {
+    title: 'Surface Water',
+  },
+};
+
+export const speciesColumnConfig = {
+  name: {
+    title: 'Name',
+    isSortable: true,
+    isSearchable: true,
+  },
+  classification: {
+    title: 'Classification',
+    isSortable: true,
+  },
+  designation: {
+    title: 'Designation',
+    isSortable: true,
+  },
+  average_height: {
+    title: 'Average_height',
+    isSortable: true,
+  },
+  average_lifespan: {
+    title: 'Average Lifespan',
+    isSortable: true,
+  },
+  hair_colors: {
+    title: 'Hair Colors',
+    isSortable: true,
+    isSearchable: true,
+  },
+  skin_colors: {
+    title: 'Skin Colors',
+    isSortable: true,
+    isSearchable: true,
+  },
+  language: {
+    title: 'Language',
+    isSearchable: true,
+  },
+};
+
+export const starshipsColumnConfig = {
+  name: {
+    title: 'Name',
+    isSortable: true,
+    isSearchable: true,
+  },
+  model: {
+    title: 'Model',
+    isSearchable: true,
+  },
+  starship_class: {
+    title: 'Starship Class',
+    isSortable: true,
+  },
+  manufacturer: {
+    title: 'Manufacturer',
+    isSortable: true,
+  },
+  cost_in_credits: {
+    title: 'Cost in credits',
+    isSortable: true,
+  },
+  length: {
+    title: 'Length',
+    isSortable: true,
+  },
+  passengers: {
+    title: 'Passengers',
+    isSearchable: true,
+  },
+  max_atmosphering_speed: {
+    title: 'Max atmosphering speed',
+    isSearchable: true,
+  },
+};
